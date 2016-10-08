3b4a5a56cb6549906104b1e88e8703a8d2cf984f
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-10-07 19:23:47 -0700

    made changes to data model

--- tinder/models.py
+++ tinder/models.py
@@ -12,9 +12,14 @@ class User(models.Model):
     bio = models.TextField(blank=True)
     schools = models.CharField(max_length=50, blank=True)
     jobs = models.CharField(max_length=50, blank=True)
+    # distance in miles
+    distance = models.FloatField(default=0.0)
 
     # social media
     instagram_username = models.CharField(max_length=30, blank=True)
     mentions_snapchat = models.BooleanField(default=False)
     mentions_kik = models.BooleanField(default=False)
     mentions_instagram = models.BooleanField(default=False)
+
+    # json representation from tinder
+    _data = models.TextField()
